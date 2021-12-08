8defc85f774f23ceef850b076ea4354446258e7b
Yelena1212 <Baranchuk1212@gmail.com>
2021-12-07 23:59:19 -0800

    interest page

--- HTML/app.js
+++ HTML/app.js
@@ -8,5 +8,39 @@ $(function () {
     $(modal_resume).on("click", function () {
         $(modal_resume).removeClass('show');
     })
+
+    // $(".photo-slider__up").slick({
+    //     arrows: false,
+    //     dots: false,
+    //     slidesToShow: 2,
+    //     slidesToScroll: 1,
+    //     variableWidth: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 516,
+    //             settings: {
+    //                 arrow: false
+    //             }
+    //         }
+    //     ]
+
+    // })
+
+    // $(".photo-slider__down").slick({
+    //     arrows: false,
+    //     dots: false,
+    //     slidesToShow: 2,
+    //     slidesToScroll: 1,
+    //     variableWidth: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 516,
+    //             settings: {
+    //                 arrow: false
+    //             }
+    //         }
+    //     ]
+
+    // })
 });
 
