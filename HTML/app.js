00a8e6b392a15f6578897ac46da5e2e02a85e4a4
Yelena1212 <Baranchuk1212@gmail.com>
2021-12-09 11:20:29 -0800

    final project done

--- HTML/app.js
+++ HTML/app.js
@@ -9,38 +9,5 @@ $(function () {
         $(modal_resume).removeClass('show');
     })
 
-    // $(".photo-slider__up").slick({
-    //     arrows: false,
-    //     dots: false,
-    //     slidesToShow: 2,
-    //     slidesToScroll: 1,
-    //     variableWidth: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 516,
-    //             settings: {
-    //                 arrow: false
-    //             }
-    //         }
-    //     ]
 
-    // })
-
-    // $(".photo-slider__down").slick({
-    //     arrows: false,
-    //     dots: false,
-    //     slidesToShow: 2,
-    //     slidesToScroll: 1,
-    //     variableWidth: true,
-    //     responsive: [
-    //         {
-    //             breakpoint: 516,
-    //             settings: {
-    //                 arrow: false
-    //             }
-    //         }
-    //     ]
-
-    // })
 });
-
